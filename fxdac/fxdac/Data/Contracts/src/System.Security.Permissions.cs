8c72dd1294e48c794d8c7a2f1cd92f89fb3e3fa9
Krzysztof Cwalina <kcwalina@microsoft.com>
2016-06-06 13:34:13 -0700

    Cleaned up Policy and Permissions contracts

--- fxdac/fxdac/Data/Contracts/src/System.Security.Permissions.cs
+++ fxdac/fxdac/Data/Contracts/src/System.Security.Permissions.cs
@@ -1246,25 +1246,3 @@ namespace System.Security.Policy {
   }
 
 } // end of System.Security.Policy
-namespace System.Security.Principal {
-  [System.Runtime.InteropServices.ComVisibleAttribute(true)]
-  public partial interface IIdentity {
-    string AuthenticationType { get; }
-    bool IsAuthenticated { get; }
-    string Name { get; }
-  }
-
-  [System.Runtime.InteropServices.ComVisibleAttribute(true)]
-  public partial interface IPrincipal {
-    System.Security.Principal.IIdentity Identity { get; }
-    bool IsInRole(string role);
-  }
-
-  [System.Runtime.InteropServices.ComVisibleAttribute(true)]
-  public enum PrincipalPolicy {
-    NoPrincipal = 1,
-    UnauthenticatedPrincipal = 0,
-    WindowsPrincipal = 2,
-  }
-
-} // end of System.Security.Principal
