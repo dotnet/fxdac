eab36f91ab0bf9b6682ddd3d964fefe136abca87
Krzysztof Cwalina <kcwalina@microsoft.com>
2016-06-06 13:51:28 -0700

    Moved X509Ceritificate APIs to a single contract

--- fxdac/fxdac/Data/Contracts/src/System.Security.Principal.cs
+++ fxdac/fxdac/Data/Contracts/src/System.Security.Principal.cs
@@ -1,4 +1,31 @@
 [assembly:System.CLSCompliant(true)]
+namespace System.Security {
+  [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+  public sealed partial class SecurityElement {
+    public SecurityElement(string tag) { }
+    public SecurityElement(string tag, string text) { }
+    public System.Collections.Hashtable Attributes { get { return default(System.Collections.Hashtable); } set { } }
+    public System.Collections.ArrayList Children { get { return default(System.Collections.ArrayList); } set { } }
+    public string Tag { get { return default(string); } set { } }
+    public string Text { get { return default(string); } set { } }
+    public void AddAttribute(string name, string value) { }
+    public void AddChild(System.Security.SecurityElement child) { }
+    public string Attribute(string name) { return default(string); }
+    [System.Runtime.InteropServices.ComVisibleAttribute(false)]
+    public System.Security.SecurityElement Copy() { return default(System.Security.SecurityElement); }
+    public bool Equal(System.Security.SecurityElement other) { return default(bool); }
+    public static string Escape(string str) { return default(string); }
+    public static System.Security.SecurityElement FromString(string xml) { return default(System.Security.SecurityElement); }
+    public static bool IsValidAttributeName(string name) { return default(bool); }
+    public static bool IsValidAttributeValue(string value) { return default(bool); }
+    public static bool IsValidTag(string tag) { return default(bool); }
+    public static bool IsValidText(string text) { return default(bool); }
+    public System.Security.SecurityElement SearchForChildByTag(string tag) { return default(System.Security.SecurityElement); }
+    public string SearchForTextOfTag(string tag) { return default(string); }
+    public override string ToString() { return default(string); }
+  }
+
+} // end of System.Security
 namespace System.Security.Principal {
   [System.Runtime.InteropServices.ComVisibleAttribute(true)]
   public partial interface IIdentity {
