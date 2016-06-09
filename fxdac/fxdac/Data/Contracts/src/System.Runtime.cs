4078a838e300a33d553ea89d5e4170d9125d6ab3
Krzysztof Cwalina <kcwalina@microsoft.com>
2016-06-09 13:32:19 -0700

    Small Cleanup

--- fxdac/fxdac/Data/Contracts/src/System.Runtime.cs
+++ fxdac/fxdac/Data/Contracts/src/System.Runtime.cs
@@ -8982,4 +8982,15 @@ namespace System.ComponentModel {
     Never = 1,
   }
 
+  public partial class Win32Exception : System.Runtime.InteropServices.ExternalException, System.Runtime.Serialization.ISerializable {
+    public Win32Exception() { }
+    public Win32Exception(int error) { }
+    public Win32Exception(int error, string message) { }
+    protected Win32Exception(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) { }
+    public Win32Exception(string message) { }
+    public Win32Exception(string message, System.Exception innerException) { }
+    public int NativeErrorCode { get { return default(int); } }
+    public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) { }
+  }
+
 } // end of System.ComponentModel
